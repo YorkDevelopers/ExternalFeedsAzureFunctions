f681d0a6372322d5f106e572a7704cf3e99e7bb8
York Developers <code@yorkdevelopers.org>
2018-04-25 07:55:00 +0100

    added eventcounter.yml file generation

---
+++ Feeds/EventCounter/CounterList.cs
@@ -0,0 +1,11 @@
+﻿namespace Feeds.EventCounter
+{
+    public class CounterList
+    {
+        public int Meetups_2018 { get; set; }
+        public int Meetups_2017 { get; set; }
+        public int Meetups_This_Month { get; set; }
+        public int Meetups_This_Week { get; set; }
+
+    }
+}
---
+++ Feeds/EventCountersFunction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Feeds.EventCounter;
+using YamlDotNet.Serialization;
+using Feeds.Shared;
+
+namespace Feeds
+{
+    class EventCountersFunction
+    {
+        [FunctionName("EventCounters")]
+        public static void Run([TimerTrigger("0 12 3 * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            var eventCounterList = new CounterList();
+            eventCounterList.Meetups_2018 = 100;
+            eventCounterList.Meetups_2017 = 33;
+            eventCounterList.Meetups_This_Month = 10;
+            eventCounterList.Meetups_This_Week = 3;
+
+            var serializer = new Serializer();
+            var yaml = serializer.Serialize(eventCounterList);
+
+            // Push the file to git
+            var gitHubClient = new GitHub(log);
+            gitHubClient.WriteFileToGitHub("_data/EventCounter.yml", yaml);
+
+        }
+    }
+}
