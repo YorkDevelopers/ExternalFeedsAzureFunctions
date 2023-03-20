734f25fafa5f25325d244ce63002974904737c9a
davidbetteridge <david.betteridge@zeromission.io>
2023-03-20 09:15:09 +0000

    Update references

--- Feeds/EventBriteFunction.cs
+++ Feeds/EventBriteFunction.cs
@@ -9,6 +9,15 @@ using Feeds.Shared;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using YamlDotNet.Serialization;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Feeds
 {
